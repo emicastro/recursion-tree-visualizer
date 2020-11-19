0f9c5500e19760825133dac1c9ad14df856d75fb
Emilio Castro <emilio.castro@mercadolibre.com>
2020-11-19 15:49:34 -0300

    Adds Icon files

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as FirstIcon } from './first.svg'
+export { default as LastIcon } from './last.svg'
+export { default as Logo } from './logo.svg'
+export { default as PreviousIcon } from './previous.svg'
+export { default as NextIcon } from './next.svg'
