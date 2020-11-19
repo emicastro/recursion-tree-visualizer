babb1af4e0fa016be4158665befe16248bbda810
Emilio Castro <emilio.castro@mercadolibre.com>
2020-11-19 15:06:41 -0300

    Adds types necesary for display the graph component

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,21 @@
 export type Point = [number, number] // [x, y]
+
+// key: node id
+export type NodesData = Record<
+  number,
+  {
+    times: number[] // times that a node is the current (in incremental order)
+    coord: Point
+    label?: string
+    memorized: boolean
+  }
+>
+
+// key = JSON.stringify([m, n]), for edge m -> n
+export type EdgesData = Record<
+  string,
+  {
+    timeRange: [number, number] // Min/Max intervals in which edge should be displayed
+    label?: string
+  }
+>
