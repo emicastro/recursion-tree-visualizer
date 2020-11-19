c060b1fe6af29580a0a3c0ba386d030715cbce06
Emilio Castro <emilio.castro@mercadolibre.com>
2020-11-19 16:02:57 -0300

    Adds index to component folder to better exporting of components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Footer } from './footer'
+export { default as Graph } from './graph'
+export { default as LogBar } from './log-bar'
+export { default as ProgressBar } from './progress-bar'
